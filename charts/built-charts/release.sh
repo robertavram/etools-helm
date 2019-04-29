6ac7ce70c587fd99d08c6eec31ffc4ea7fd23971
robertavram <robert.avram@gmail.com>
2019-04-29 14:38:18 -0400

    Up script

--- charts/built-charts/release.sh
+++ charts/built-charts/release.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
+
 repos=(django t2f pmp ap action-points tpm dashboard)
 CHARTS_PATH="$(dirname "$(pwd)")"
 echo "What release number do you want to use?"
-# read VERSION
+read VERSION
 
 
 
@@ -13,10 +14,10 @@ do
     REPO_PTH=${CHARTS_PATH}/etools-${REPO}
     echo $REPO_PTH
     helm install --dry-run --debug $REPO_PTH
-    # echo "packaging ${REPO} with verion ${VERSION}"
-    # helm package --version $VERSION REPO_PTH
-    # echo "pushing image to GCS"
-    # echo "helm gcs push etools-${REPO}:${VERSION} etools-charts"
+    echo "packaging ${REPO} with verion ${VERSION}"
+    helm package --version $VERSION $REPO_PTH
+    echo "pushing image to GCS"
+    echo "helm gcs push etools-${REPO}:${VERSION} etools-charts"
 done
 
 
